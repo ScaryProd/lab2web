88314291f35b40cabe78e73e997eb6dd77aa2000
Alvaro Santana <alvaro.santana23@gmail.com>
2020-09-21 01:08:27 -0500

    yajala!

--- utils.js
+++ utils.js
@@ -23,54 +23,65 @@ let total = Number("0");
 
 async function getPokemon(pokemonValue) {
   try {
-    axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonValue}`).then(response => {
-    var pokemon = response.data
-    return pokemon;
-    // El weight esta en pokemon.weight
-    })
+    let pokemonData = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonValue}`);
+    return pokemonData;
+  } catch (error) {
+    console.error(error);
+  }
   
+}
+
+async function callSnorlax() {
+  try {
+    const response = await axios.get('https://pokeapi.co/api/v2/pokemon/snorlax');
+    console.log(response.data.weight);
   } catch (error) {
     console.error(error);
   }
 }
-
 // Create a new list item when clicking on the "Add" button
-function newElement() {
+async function newElement() {
 
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
   var pokemonValue = inputValue;
 
+  let pokemonData;
   var totalVar = document.getElementById("totalSpan");
-  (async () => {
-    let pokemonData;
-    pokemonData = getPokemon(pokemonValue);
-    inputCost = pokemonData.weight;
-    var t = document.createTextNode(pokemonValue);
-    var cost = document.createTextNode(inputCost);
-    var costSpan = document.createElement("span");
-    costSpan.className = "cost"
-    costSpan.append(cost)
-    li.appendChild(t);
-    //li.appendChild(tab);
-    li.appendChild(costSpan);
-    if (inputValue === '') {
-      document.getElementById("myDIV").style.backgroundColor = "red";
-    } else {
-      document.getElementById("myDIV").style.backgroundColor = "transparent";
-      total += Number(inputCost);
-      document.getElementById("myUL").appendChild(li);
-    }
-    document.getElementById("myInput").value = "";
-    totalVar.innerText = total;
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-  })()
+  
+  try {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonValue}`);
+    console.log(response.data.weight);
+    pokemonData = response;
+  } catch (error) {
+    console.error(error);
+  }
+  inputCost = pokemonData.data.weight;
+  var t = document.createTextNode(pokemonValue);
+  var cost = document.createTextNode(inputCost);
+  var costSpan = document.createElement("span");
+  costSpan.className = "cost"
+  costSpan.append(cost)
+  li.appendChild(t);
+  //li.appendChild(tab);
+  li.appendChild(costSpan);
+  if (inputValue === '') {
+    document.getElementById("myDIV").style.backgroundColor = "red";
+  } else {
+    document.getElementById("myDIV").style.backgroundColor = "transparent";
+    total += Number(inputCost);
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+  totalVar.innerText = total;
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
 
-  inputCost = 50;
+
+  //inputCost = 50;
   
   //para quitar el elemento con tachita
   for (i = 0; i < close.length; i++) {
